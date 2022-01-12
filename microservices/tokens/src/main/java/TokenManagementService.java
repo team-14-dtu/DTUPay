ca3e42f0c483b8fe1066a96f2ce846e185233eaf
Mathilde T <55983203+s184376@users.noreply.github.com>
2022-01-12 13:56:34 +0100

    add bank generation to run script

---
+++ microservices/tokens/src/main/java/TokenManagementService.java
@@ -0,0 +1,15 @@
+public class TokenManagementService {
+    MessageQueue queue;
+
+    public TokenManagementService(MessageQueue q) {
+        this.queue = q;
+        this.queue.addHandler("StudentRegistrationRequested", this::handleStudentRegistrationRequested);
+    }
+
+    public void handleStudentRegistrationRequested(Event ev) {
+        var s = ev.getArgument(0, Student.class);
+        s.setId("123");
+        Event event = new Event("StudentIdAssigned", new Object[] { s });
+        queue.publish(event);
+    }
+}
