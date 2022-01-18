88c574fd8ff85f5f397e6127933ad7f3e145670f
Petr Kubeš <petr.kubes@applifting.cz>
2022-01-18 14:08:12 +0100

    fix install.sh to prevent nuking everyones computers

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,12 @@ COLOR='\033[0;33m'
 NC='\033[0m' # No Color
 printf "${COLOR} --------- Installing dependencies. --------- ${NC}\n"
 
+if [[ "$PWD" == *scripts ]]
+then
+  cd ..
+fi
+
+
 pushd libs
 pushd messaging-utilities
 ./install.sh
